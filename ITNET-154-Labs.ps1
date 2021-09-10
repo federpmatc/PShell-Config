43e9f7d582a927cd974997a71ce6a6c3fbcfa46e
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-09-10 08:37:39 -0500

    Update ITNET-154-Labs.ps1

--- ITNET-154-Labs.ps1
+++ ITNET-154-Labs.ps1
@@ -1,1 +1,4 @@
+#Chapter 9, Question 3
+get-adcomputer -filter  {name -like '*Server*' } | Select-Object -property @{l='computername';e={$_.name}} | Get-Hotfix
 
+Get-Hotfix -computername Server2019-1,Server2019-2
