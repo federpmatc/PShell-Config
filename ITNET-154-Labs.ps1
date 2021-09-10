c179cb5a88ee0843a838c6c3bcd893e4db4ad10b
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-09-10 08:40:53 -0500

    Update ITNET-154-Labs.ps1

--- ITNET-154-Labs.ps1
+++ ITNET-154-Labs.ps1
@@ -1,3 +1,7 @@
+#Disable firewall on Server2019-1 & Server2019-2
+#This requires launching PowerShell with Administrative privileges
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
 #Chapter 9, Question 3
 get-adcomputer -filter  {name -like '*Server*' } | Select-Object -property @{l='computername';e={$_.name}} | Get-Hotfix
 
