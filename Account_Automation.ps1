6fadf0ae490424296e51d50ae836a9a5bbbf3fed
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-10-05 10:27:43 -0500

    Update Account_Automation.ps1

--- Account_Automation.ps1
+++ Account_Automation.ps1
@@ -1,8 +1,10 @@
 
 #Task 1
+
 New-ADUser -SamAccountName “SeanG” -Name “Sean G” -GivenName “Sean” -Surname “G” -Path “ou=Staff,ou=IT Network specialist,dc=ITNET,dc=pri” -AccountPassword (ConvertTo-SecureString Password01 -AsPlainText -force) -PasswordNeverExpires $true -Enabled $true
 
 #Task 2
-https://drive.google.com/open?id=0B7aOXOfOltnbbVZJaW0teEZjZDg
-
+Notepad C:\PowerShell\Users.csv
+import-csv C:\PowerShell\users.csv
+import-csv C:\PowerShell\users.csv | Foreach {New-ADUser -SamAccountName $_.samAccountName -Name $_.name -GivenName $_.givenName -Surname $_.surname -Path $_.path -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -force) -PasswordNeverExpires $true -Enabled $true} 
 
